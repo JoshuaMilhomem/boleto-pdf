08a7a43dfcae030552cf0b75595a3c00b573a74b
Joshua M <joshuamilhomem@gmail.com>
2024-03-01 16:10:57 -0300

    Update format-date.js

--- src/commons/utils/format-date.js
+++ src/commons/utils/format-date.js
@@ -3,5 +3,5 @@ import {DateTime} from 'luxon'
 const DEFAULT_FORMAT = 'dd/LL/yyyy'
 
 export default function (date, format = DEFAULT_FORMAT) {
-  return DateTime.fromJSDate(date).toFormat(format)
+  return date
 }
